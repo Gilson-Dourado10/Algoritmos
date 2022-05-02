0326b836e42e16758632a466d925953b11b71d68
gilson dourado <gilsondourado10@gmail.com>
2022-05-01 23:19:47 -0300

    line search

--- teste.rb
+++ teste.rb
@@ -3,3 +3,43 @@ def hello
 end 
 puts hello
 
+class A
+    @@contexto = "classe"
+
+    def initialize
+        @contexto = "instância"
+    end
+
+    def contexto
+        @contexto
+    end
+
+    def A.contexto
+        @@contexto
+    end
+end
+
+a = A.new
+a.contexto  # >> "instância"
+A.contexto  # >> "classe"
+
+
+class A
+    @@contexto = "classe"
+
+    def initialize
+        @contexto = "instância"
+    end
+
+    def contexto
+        @contexto
+    end
+
+    def A.contexto
+        @@contexto
+    end
+end
+
+a = A.new
+a.contexto  # >> "instância"
+A.contexto  # >> "classe"
